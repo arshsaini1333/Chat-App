e96da3c293578a8f1454204eef337175de9c2328
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-03-10 18:36:08 +0530

    REgister Page

--- public/src/Pages/Register.jsx
+++ public/src/Pages/Register.jsx
@@ -1,43 +1,72 @@
 import logo from "../assets/logo.svg";
-import { handleChange, handleSubmit } from "../Utils/Register";
+// import { handleChange, handleSubmit, stateVar } from "../Utils/Register";
+import { handleValidation } from "../Utils/Register";
 import "../public/Register.css";
+import { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    //console.log(event.target.value);
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (handleValidation(values)) {
+      console.log(values);
+    }
+  };
+
   return (
-    <div className="Register">
-      <form onSubmit={handleSubmit}>
-        <div className="brand">
-          <img src={logo} alt="Logo" />
-          <h1>Chit-Chat</h1>
-        </div>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          name="confermPassword"
-          onChange={handleChange}
-        />
-        <button type="submit">Create User</button>
-        <span>
-          Already have an account ? <a href="/login">Login</a>
-        </span>
-      </form>
-    </div>
+    <>
+      <div className="Register">
+        <form onSubmit={handleSubmit}>
+          <div className="brand">
+            <img src={logo} alt="Logo" />
+            <h1>Chit-Chat</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={values.confirmPassword}
+            onChange={handleChange}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <a href="/login">Login</a>
+          </span>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
   );
 }
--- public/src/Utils/Register.js
+++ public/src/Utils/Register.js
@@ -1,7 +1,40 @@
-export const handleChange = (event) => {
-  console.log(event.target.value);
+import { toast, ToastContainer } from "react-toastify";
+
+// export const handleChange = (event) => {
+//   //console.log(event.target.value);
+//   setValues({ ...values, [event.target.name]: event.target.value });
+// };
+// export const handleSubmit = (event) => {
+//   event.preventDefault();
+//   console.log(values);
+//   alert("Form");
+// };
+
+const toastOpt = {
+  position: "bottom-right",
+  autoClose: 8000,
+  pauseOnHover: true,
+  draggable: true,
+  theme: "dark",
 };
-export const handleSubmit = (event) => {
-  event.preventDefault();
-  alert("Form");
+
+export const handleValidation = (values) => {
+  const { password, confirmPassword, username, email } = values;
+  if (username.length < 5) {
+    toast.error("Username should be Greater than 5 Character", toastOpt);
+    return false;
+  } else if (email === "") {
+    toast.error("Email is Required", toastOpt);
+  } else if (password.length < 8) {
+    toast.error(
+      "Password should be equal and Greater than 8 Character",
+      toastOpt
+    );
+    return false;
+  } else if (password !== confirmPassword) {
+    toast.error("Password and Confirm Password should be same.", toastOpt);
+    return false;
+  } else {
+    return true;
+  }
 };
