4430a0da69d448e2b438b083a47be82138d4d5bb
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-03-24 22:12:35 +0530

    New Changes

--- public/src/Utils/APIRoutes.js
+++ public/src/Utils/APIRoutes.js
@@ -1,4 +1,5 @@
 export const host = "chat-app-by-arsh.vercel.app";
+// export const host = "http://localthost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
